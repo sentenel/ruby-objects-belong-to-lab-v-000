ee484a3117a51ae54fb8c9df408c3c3a0bcad437
Seth Wax <sethlwax@gmail.com>
2018-04-05 01:30:26 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :title, :artist
+
+  def initialize(title)
+    @title = title
+  end
+end
+
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
